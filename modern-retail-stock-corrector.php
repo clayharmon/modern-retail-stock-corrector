e086361d21c26860f2c372302d4fb172a1c23bc1
Clay Harmon <clay@clayharmon.me>
2021-12-16 09:22:01 +0300

    Add settings page to admin menu

--- modern-retail-stock-corrector.php
+++ modern-retail-stock-corrector.php
@@ -14,3 +14,25 @@ $cc_mrsc_update_checker = Puc_v4_Factory::buildUpdateChecker(
 	'cc-mrsc'
 );
 $cc_mrsc_update_checker->getVcsApi()->enableReleaseAssets();
+
+function cc_mrsc_add_settings_page(){
+  add_submenu_page(
+    'tools.php',
+    'Fix Stocks',
+    'Fix Stocks',
+    'manage_options',
+    'cc-mrsc',
+    'cc_mrsc_settings_html'
+  );
+}
+add_action( 'admin_menu', 'cc_mrsc_add_settings_page' );
+
+function cc_mrsc_settings_html() {
+	?>
+  <div class="wrap">
+    <h1 class="wp-heading-inline">Fix Stocks</h1>
+    <hr class="wp-header-end">
+  </div>
+  <?php
+}
+?>
